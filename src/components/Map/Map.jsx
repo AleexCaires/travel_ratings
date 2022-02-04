ec5030322218dcf82c15e96ae13a57f64b871537
AleexCaires <tiagoaleex98@gmail.com>
2022-02-04 13:01:53 +0000

    Added Logic and props to GoogleMapReact

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -7,8 +7,32 @@ import Rating from '@material-ui/lab'
 import useStyles from './styles'
 
 const Map = () => {
+    const classes = useStyles();
+    // this means that the isMobile Variable is gonna be set to false if the width of the device is larger than 600px
+    const isMobile = useMediaQuery('(min-width:600px)')
+    
+    const coordinates = { lat : 0, lon: 0};
+
+
     return( 
-        <h1>Map</h1>
+        <div>
+            <div className={classes.mapContainer}>
+                <GoogleMapReact
+                    bootstrapURLKeys={{ key: ''}}
+                    defaultCenter={coordinates}
+                    center={coordinates}
+                    defaultZoom={14}
+                    margin={[50,50,50,50]}
+                    options={''}
+                    onChange={''}
+                    onChildClick={''}
+                
+                >
+
+                </GoogleMapReact>
+
+            </div>
+        </div>
     )
 }
 
